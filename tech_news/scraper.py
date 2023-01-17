43f2bf9f38d1ae04b99e0967c3aee0609e10e3cc
ytellon <ytellonmacedo@gmail.com>
2023-01-17 16:19:11 -0300

    feat: Python | scrape next page html

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -25,7 +25,11 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page = selector.css('a.next.page-numbers::attr(href)').get()
+    if next_page:
+        return next_page
+    return None
 
 
 # Requisito 4
