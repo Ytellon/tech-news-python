fcea075922c8d18da3871d323e713c57f593de6d
ytellon <ytellonmacedo@gmail.com>
2023-01-18 15:33:20 -0300

    feat: Python | scrape news requests

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -19,14 +19,14 @@ def fetch(url):
 # Requisito 2
 def scrape_updates(html_content):
     selector = Selector(html_content)
-    urls = selector.css('h2.entry-title a::attr(href)').getall()
+    urls = selector.css("h2.entry-title a::attr(href)").getall()
     return urls
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
     selector = Selector(html_content)
-    next_page = selector.css('a.next.page-numbers::attr(href)').get()
+    next_page = selector.css("a.next.page-numbers::attr(href)").get()
     if next_page:
         return next_page
     return None
@@ -34,7 +34,20 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return {
+        "url": selector.css('link[rel="canonical"]::attr(href)').get(),
+        "title": selector.css("h1.entry-title::text").get().strip(),
+        "timestamp": selector.css(".meta-date::text").get(),
+        "writer": selector.css(".author a::text").get(),
+        "comments_count": len(selector.css("ol.comment-list li").getall())
+        or 0,
+        "summary": "".join(
+            selector.css(".entry-content > p:nth-of-type(1) *::text").getall()
+        ).strip(),
+        "tags": selector.css(".post-tags li a::text").getall(),
+        "category": selector.css(".category-style span.label::text").get(),
+    }
 
 
 # Requisito 5
